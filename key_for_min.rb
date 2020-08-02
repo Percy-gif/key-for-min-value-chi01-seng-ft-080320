c2c99bda3ab59aaec6d7911fd5d31f7504aecc4b
Percy Major <chipercy@hotmail.com>
2020-08-02 22:04:45 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,23 +1,22 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(hash)
+def key_for_min_value(name_hash)
 
   #tracking variables 
   min_key = nil 
   min_value = nil 
 
   #iterator
-  hash.each do |key, value|
+  name_hash.each do |key, value|
     if min_value == nil 
       min_key = key 
       min_value = value 
-    else 
+    elsif  
     end 
   end 
 
   #return min or nil 
   min_key  
   end
-  
-  key_for_min_value
+ 
