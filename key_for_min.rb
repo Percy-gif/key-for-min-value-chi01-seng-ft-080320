50401b837eee63ad735734f8d25450ba152e6262
Percy Major <chipercy@hotmail.com>
2020-08-02 21:58:43 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,14 +2,22 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(hash)
-  
-index = 0 
-min_key = nil 
-min_value = nil 
 
+  #tracking variables 
+  min_key = nil 
+  min_value = nil 
+
+  #iterator
   hash.each do |key, value|
-      if  
-      end
-  
+    if min_value == nil 
+      min_key = key 
+      min_value = value 
+    else 
+    end 
   end 
- 
+
+  #return min or nil 
+  min_key  
+  end
+  
+  key_for_min_value
